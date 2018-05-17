d6d7dabe7ca83ae4fddb61e1ebd11aca4e2a4ae0
GuiBret <guillaume.bretzner@gmail.com>
2018-05-17 16:45:42 +0100

    fix(urls): bugfix in urls
    Fix bug which would put "//" in the required URL to get the albums' list

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^/', views.albums, name="listing"),
+    url(r'^albums/$', views.albums, name="listing"),
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name="detail"),
     url(r'^search/', views.search, name="search")
 ]
