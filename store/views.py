cb0151df9a2ea94c508978aef284ae19f5d6caee
GuiBret <guillaume.bretzner@gmail.com>
2018-05-17 16:39:53 +0100

    refactor(views): use render + removed useless code
    Change returns so that they use django.shortcuts.render
    
    Remove code creating HTML on the view since we now have a theme

--- store/views.py
+++ store/views.py
@@ -1,32 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Album, Artist, Contact, Booking
-from django.template import loader
+
 
 
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('store/index.html')
 
     albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
 
-    return HttpResponse(template.render({"albums": albums}, request=request))
+    return render(request, "store/index.html", {"albums": albums})
 
 def albums(request):
     albums = Album.objects.filter(available=True)
-    list_albums = ["<li>{}</li>".format(album.title) for album in albums]
 
-    message = """<ul>{}</ul>""".format("\n".join(list_albums))
-    return HttpResponse(message)
+    return render(request, "store/listing.html", {"albums": albums})
 
 def detail(request, album_id):
     album = Album.objects.get(pk=album_id)
     artists = " ".join(artist.name for artist in album.artists.all())
-
-    message = "Le nom de l'album est {}. Il a été écrit par {}.".format(album.title, artists)
-
-    return HttpResponse(message)
+    
+    return render(request, "store/detail.html", {"album": album, "artists":artists})
 
 def search(request):
     query = request.GET.get('query')
